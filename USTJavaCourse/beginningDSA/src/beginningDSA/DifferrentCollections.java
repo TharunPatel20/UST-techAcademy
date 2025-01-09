759cc291321f7f69c9ce83838a8d3f36079457ea
tharun <bonthalatharun@gmail.com>
2025-01-09 17:32:25 +0530

    Started java course provided by UST in TechAcademy.com

---
+++ USTJavaCourse/beginningDSA/src/beginningDSA/DifferrentCollections.java
@@ -0,0 +1,18 @@
+package beginningDSA;
+
+
+/**
+*
+*1. Create a Java Application to Work with different collections - Arrays, Linked List, Double
+*Linked List and Circular Linked List
+*
+*/
+
+public class DifferrentCollections {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
